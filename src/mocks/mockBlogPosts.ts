f05dcb9d49180c611d539593d6769dccf5d3e393
rubenszinho <samuelrubenscontato@gmail.com>
2024-11-21 17:20:02 -0300

    fix: fix mock variable name

--- src/mocks/mockBlogPosts.ts
+++ src/mocks/mockBlogPosts.ts
@@ -1,5 +1,4 @@
-
-  export const _mockPostContent = `
+const mockPostContent = `
     <div>
         <p>Welcome to our latest blog post! In this article, we will explore <strong>the best practices for web development</strong> in 2024.</p>
         <img src="https://upload.wikimedia.org/wikipedia/en/a/a9/Example.jpg" alt="Web Development" style="max-width:100%; height:auto;" />
@@ -26,10 +25,9 @@ interface MockedBlogPost {
     content: string;
   }
 
-  export const generateBlogPost = (id: string) => { return { id: id, title: `Post ${id}`, summary: `This is the post ${id}.`, content: _mockPostContent,}};
-
+  export const generateBlogPost = (id: string) => { return { id: id, title: `Post ${id}`, summary: `This is the post ${id}.`, content: mockPostContent,}};
 
   export const mockBlogPosts: MockedBlogPost[] = Array.from({length: 10}, (_, i) => {
     const id = (i + 1).toString();
     return generateBlogPost(id);
-  });
+});
