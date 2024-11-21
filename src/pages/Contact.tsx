5913d34a3b1b9d75883beb63bcf8eb1930c1e68a
rubenszinho <samuelrubenscontato@gmail.com>
2024-11-21 17:17:33 -0300

    fix: standardize page links and fix component naming
    - Updated links in `Home.tsx` to use standardized route definitions.
    - Corrected `Contact` component export name from `About` to `Contact`.

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const About: React.FC = () => {
+const Contact: React.FC = () => {
     return <h1>Contact Us Page</h1>;
 };
 
-export default About;
+export default Contact;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,9 +6,9 @@ const Home: React.FC = () => {
     return (
         <div>
             <h1>Welcome to Rubrion</h1>
-            <p><Link to={ROUTES.PUBLIC.Home.path}>Home</Link>.</p>
-            <p><Link to={ROUTES.PUBLIC.About.path}>About</Link>.</p>
-            <p><Link to={ROUTES.PUBLIC.Contact.path}>Contact Us</Link>.</p>
+            <p><Link to={ROUTES.PUBLIC.HOME.path}>Home</Link>.</p>
+            <p><Link to={ROUTES.PUBLIC.ABOUT.path}>About</Link>.</p>
+            <p><Link to={ROUTES.PUBLIC.CONTACT.path}>Contact Us</Link>.</p>
             <Link to={ROUTES.BLOG.LIST.path}>Go to Blog</Link>
         </div>
     );
